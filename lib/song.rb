3f394405cbdf01565c7d27ea32f6f12b5f96150b
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-23 12:44:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,6 +35,19 @@ class Song
     end
     @@genre_hash
   end
+  
+  def self.artist_count
+    @@artists.each do |artist|
+      if @@artist_hash.key?(artist) == true
+        @@artist_hash[artist] += 1 
+      else
+        @@artist_hash.store(artist, 1)
+      end
+    end
+    @@artist_hash
+  end
+  
+        
         
       
   
